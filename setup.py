838233bf7991ae54d52c15305a00fa71aa83c07c
renzon <renzon@gmail.com>
2014-07-05 19:04:05 -0300

    fixing install_requires

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from setup import install_requires
 import codecs
 import os
 import sys
@@ -25,12 +26,12 @@ standard_exclude_directories = [
 # you can't import this from another package, when you don't know if
 # that package is installed yet.
 def find_package_data(
-    where=".",
-    package="",
-    exclude=standard_exclude,
-    exclude_directories=standard_exclude_directories,
-    only_in_packages=True,
-    show_ignored=False):
+        where=".",
+        package="",
+        exclude=standard_exclude,
+        exclude_directories=standard_exclude_directories,
+        only_in_packages=True,
+        show_ignored=False):
     """
     Return a dictionary suitable for use in ``package_data``
     in a distutils ``setup.py`` file.
@@ -99,7 +100,7 @@ def find_package_data(
                         break
                 if bad_name:
                     continue
-                out.setdefault(package, []).append(prefix+name)
+                out.setdefault(package, []).append(prefix + name)
     return out
 
 
@@ -110,7 +111,6 @@ AUTHOR = "Renzo Nuccitelli"
 AUTHOR_EMAIL = "renzo.n@gmail.com"
 URL = "https://github.com/renzon/gaeforms"
 VERSION = __import__(PACKAGE).__version__
-
 
 setup(
     name=NAME,
@@ -133,4 +133,6 @@ setup(
         "Framework :: Paste",
     ],
     zip_safe=False,
+    install_requires=['pytz>=2014.4',
+                      'Babel>=1.3']
 )
